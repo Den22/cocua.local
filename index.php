93c8c886df7f14ab8330b612fccb834082e963c9
Den22 <mishchenko90@list.ru>
2016-07-02 21:00:28 +0300

    Шаблон base

--- index.php
+++ index.php
@@ -7,9 +7,4 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
 $template = $twig->loadTemplate('index.html');
-$title = 'Название страницы';
-$text = 'Hello, world!!!';
-$class = 'success';
-echo $template->render([
-    'title' => $title
-]);
+echo $template->render([]);
