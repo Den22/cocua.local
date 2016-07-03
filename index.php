a45f8f71d42b4469f5be3c61dd4af1250d20ab23
Den22 <mishchenko90@list.ru>
2016-07-03 21:50:15 +0300

    Шаблон base

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-$template = $twig->loadTemplate('index.html');
+$template = $twig->loadTemplate('rules.html');
 echo $template->render([]);
