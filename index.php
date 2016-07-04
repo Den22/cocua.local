3fb21bf0f304df95f89674130ab547ba814719dc
Den22 <mishchenko90@list.ru>
2016-07-04 15:43:57 +0300

    Шаблоны

--- index.php
+++ index.php
@@ -6,5 +6,7 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-$template = $twig->loadTemplate('rules.html');
+//$template = $twig->loadTemplate('rules.html');
+//$template = $twig->loadTemplate('clanlist.html');
+$template = $twig->loadTemplate('blacklist.html');
 echo $template->render([]);
